964eaedeeb6bf71d11a4fd30051e3b33a8f748b5
Neo Wei <NWEI.22@ichat.sp.edu.sg>
2023-05-03 09:58:48 +0800

    wk 3 modification

--- bmi.py
+++ bmi.py
@@ -7,11 +7,14 @@ def calculate_bmi(weight, height):
 
     if (bmi < 18.5):
         print("Under Weight")
+        return -1
 
     if (bmi > 25.0):
         print("Over Weight")
+        return 1
 
     if (18.5 < bmi > 25.0):
         print("Normal Weight")
+        return 0
 
 calculate_bmi(weight=57, height=1.82)
